3979ec53080b842c20d221a167a4f8565efa5f83
setup_git_prefs <setup_git_proxy>
2016-07-21 14:22:59 +1200

    updat

--- js/script.js
+++ js/script.js
@@ -5,4 +5,12 @@ $(document).ready(function(){
 	   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 	});
 
+	$('#new-project-button').click(function() { 
+   		document.location = 'newProject.html';
+    });
+
+	$('#back-to-feed').click(function() { 
+    	document.location = 'feed.html';
+	});
+
 });

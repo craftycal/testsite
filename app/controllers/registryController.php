59b5d5af29c04948e653b69a010aa2a0c9da50f4
setup_git_prefs <setup_git_proxy>
2016-08-13 18:54:57 +1200

    update

--- app/controllers/registryController.php
+++ app/controllers/registryController.php
@@ -1,51 +1,66 @@
 <?php 
 
-class loginController extends page{
 
-	public function __construct($database){
-
-		$this->database = $database;
-
-		if( isset( $_POST['registry'] ) ) {
-			$this->processRegistrationForm();
-		}
-	}
-
-	private function processRegistrationForm() {
+	function processRegistrationForm( $database) {
 		
 		$totalErrors = 0;
 
-		// Make sure the E-Mail has been provided
-		// and is valid
-		if( $_POST['email'] == '' ) {
-			// E-Mail is invalid
-			$this->emailMessage = 'Invalid E-Mail';
+		if( strlen( $_POST['email']) == 0 ) {
+			$data->emailMessageRegistry = '<span><i class="fa fa-bell-o" aria-hidden="true"></i></span> you must provide a email address';
 			$totalErrors++;
 		}
 
-		// Make sure the E-Mail is not in use
-		$filteredEmail = $this->database->real_escape_string( $_POST['email'] );
+		if( strlen( $_POST['username']) == 0 ) {
+			$data->usernameMessageRegistry = '<span><i class="fa fa-bell-o" aria-hidden="true"></i></span> you must provide a username';
+			$totalErrors++;
+		}
+
+		if( strlen( $_POST['password']) == 0 ) {
+			$data->passwordMessageRegistry = '<span><i class="fa fa-bell-o" aria-hidden="true"></i></span> you must provide a password';
+			$totalErrors++;
+		}	
+
+		// if ( $_POST['terms'] is checked carry on if not 
+		// $data[->termsMessage = '<span><i class="fa fa-bell-o" aria-hidden="true"></i></span> you must accept the Terms and conditions before you are allowed access to the TestSite';	
+
+		// is email already in use
+		$filteredEmail = $database->real_escape_string( $_POST['email'] );
 
 		$sql = "SELECT email
 				FROM users
 				WHERE email = '$filteredEmail'  ";
 
-		// Run the query
-		$result = $this->database->query($sql);
+		$result = $database->query($sql);
 
-		// If the query failed OR there is a result
 		if( !$result || $result->num_rows > 0 ) {
-			$this->emailMessage = 'E-Mail in use';
+			$data->emailMessageRegistry = '<span><i class="fa fa-bell-o" aria-hidden="true"></i></span> e-Mail already in use';
 			$totalErrors++;
 		}
+
+		// is username already in use
+		$filteredUsername = $database->real_escape_string( $_POST['username'] );
+
+		$sql = "SELECT username
+				FROM users
+				WHERE username = '$filteredUsername'  ";
+
+		$result = $database->query($sql);
+
+		if( !$result || $result->num_rows > 0 ) {
+			$data->usernameMessageRegistry = '<span><i class="fa fa-bell-o" aria-hidden="true"></i></span> sorry username already in use';
+			$totalErrors++;
+		}
+
 
 
 		// If the password is less than 8 characters long
 		if( strlen($_POST['password']) < 8 ) {
 			// Password is too short
-			$this->passwordMessage = '* must be at least 8 characters';
+			$data->passwordMessageRegistry = '<span><i class="fa fa-bell-o" aria-hidden="true"></i></span> must be at least 8 characters';
 			$totalErrors++;
 		}
+
+
 
 		// Determine if this data is valid to go into the database
 		if( $totalErrors == 0 ) {
@@ -54,21 +69,21 @@ class loginController extends page{
 			$hash = password_hash( $_POST['password'], PASSWORD_BCRYPT );
 
 			// Prepare the SQL
-			$sql = "INSERT INTO users (email, password)
-					VALUES ('$filteredEmail', '$hash')";
+			$sql = "INSERT INTO users (username, email, password)
+					VALUES ('$filteredUsername', '$filteredEmail', '$hash')";
 
 			// Run the query
-			$this->database->query($sql);
+			$database->query($sql);
 
 			// Check to make sure this worked
 
 
 			// Log the user in
-			$_SESSION['id'] = $this->database->insert_id;
+			$_SESSION['id'] = $database->insert_id;
 			$_SESSION['privilege'] = 'member';
 
 			// Redirect the user to their stream page
-			header('Location: index.php?page=stream');
+			header('Location: ?page=feed');
 
 	}
 }
--- app/templates/landingPage.php
+++ app/templates/landingPage.php
@@ -1,40 +1,54 @@
 <?php 
 
 	require_once ( APPROOT . 'app/controllers/loginController.php' );
+	require_once ( APPROOT . 'app/controllers/registryController.php' );
 
 	$data = processLogInRequest( $this->_database );
-
 ?>
 
 
 		<div class="login-page">
 
-		  <div class="form">
-		  	<img id="landing-logo" src="img/logo-ts.png" alt="test site logo">
+			<div class="form">
+		  		<img id="landing-logo" src="img/logo-ts.png" alt="test site logo">
 
-		    <form action="?p=landing" method="post" class="register-form">
-		      <input type="text" name="username" placeholder="username"/>
-		      <input type="password" name="password" placeholder="password"/>
-		      <input type="text" name="email" placeholder="email address"/>
-		      <input type="checkbox" name="terms" id="tac_check"><label id="tac_message">agree to terms and conditions</label>
-		      <input type="submit" class="button_style" name="create" value="create">
-		      <p class="message">Already registered? <a href="#">Sign In</a></p>
-		    </form>
-		    
-		    <form action="index.php" method="post" class="login-form">
-		  			<?php if( isset ($data['usernameMessage']) ){ ?>
-		    			<p class="alert_comment" > <?= $data['usernameMessage'] ?> </p>
-		    		<?php } ?>			    
-			    <input type="text" name="username" placeholder="username"/>	
-		    		<?php if( isset ($data['passwordMessage']) ){ ?>
-		    			<p class="alert_comment" > <?= $data['passwordMessage'] ?> </p>
-		    		<?php } ?>			    	    
-			    <input type="password" name="password" placeholder="password"/>
-					<?php if( isset($data['loginMessage']) ){ ?>
-						<p class="alert_comment" > <?= $data['loginMessage'] ?> </p>
-					<?php } ?>		    					    
-			    <input type="submit" class="button_style" name="login" value="Login">
-			    <p class="message">Not registered? <a href="#">Create an account</a></p>
-		    </form>
-		  </div>
+				<form action="?p=landing" method="post" class="registry">
+						<?php if( isset ($data['usernameMessageRegistry']) ){ ?>
+							<p class="alert_comment" > <?= $data['usernameMessageRegistry'] ?> </p>
+						<?php } ?>			   
+					<input type="text" name="username" placeholder="username"/>
+						<?php if( isset ($data['passwordMessageRegistry']) ){ ?>
+							<p class="alert_comment" > <?= $data['passwordMessageRegistry'] ?> </p>
+						<?php } ?>	
+					<input type="password" name="password" placeholder="password"/>
+						<?php if( isset ($data['emailMessageRegistry']) ){ ?>
+							<p class="alert_comment" > <?= $data['emailMessageRegistry'] ?> </p>
+						<?php } ?>	
+					<input type="text" name="email" placeholder="email address"/>
+						<?php if( isset ($data['termsMessageRegistry']) ){ ?>
+							<p class="alert_comment" > <?= $data['termsMessageRegistry'] ?> </p>
+						<?php } ?>	
+					<input type="checkbox" name="terms" id="tac_check"><label id="tac_message">agree to terms and conditions</label>
+					<input type="submit" class="button_style" name="create" value="create">
+					<p class="message">Already registered? <a href="#">Sign In</a></p>
+				</form>
+
+				<form action="index.php" method="post" class="login-form">
+					<?php if( isset ($data['usernameMessage']) ){ ?>
+						<p class="alert_comment" > <?= $data['usernameMessage'] ?> </p>
+					<?php } ?>
+					<input type="text" name="username" placeholder="username"/>	
+						<?php if( isset ($data['passwordMessage']) ){ ?>
+							<p class="alert_comment" > <?= $data['passwordMessage'] ?> </p>
+						<?php } ?>	
+					<input type="password" name="password" placeholder="password"/>
+						<?php if( isset($data['loginMessage']) ){ ?>
+							<p class="alert_comment" > <?= $data['loginMessage'] ?> </p>
+						<?php } ?>	
+
+					<input type="submit" class="button_style" name="login" value="Login">
+					<p class="message">Not registered? <a href="#">Create an account</a></p>
+				</form>
+			</div>
 		</div>
+	
